e452c28e39c0311e39e28e7694ed05f1f5e911cb
grig0ryyy <mahal7777@gmail.com>
2023-03-30 15:00:46 +0300

    universities parser update

--- src/universities/services/parser.py
+++ src/universities/services/parser.py
@@ -1,6 +1,8 @@
 import json
 from abc import ABC, abstractmethod
 
+from src.universities.models import University
+
 
 class UniversitiesParserCommand(ABC):
     @abstractmethod
@@ -14,18 +16,21 @@ class UniversitiesParserCommandImpl(UniversitiesParserCommand):
     def __call__(self):
         with open(self._get_file()) as file:
             universities = json.loads(file.read())
-        self._split_universities(universities=universities)
+        self._split_universities(universities_data=universities)
 
     @staticmethod
     def _get_file():
-        return 'universities/services/universities_base/universities_base.txt'
+        return 'universities/services/universities_base/universities_base.txt'  # TODO mb env
 
-    def _split_universities(self, universities):  # TODO maybe bulk create
-        for university in universities:
-            print(university)  # TODO DELETE
-            # TODO Save university
+    def _split_universities(self, universities_data):
+        universities = [
+            University(
+                name=university_data['name'],
+                country=university_data['country'],
+                alpha_two_code=university_data['alpha_two_code'],
+                web_page=university_data['web_pages'][0] if university_data['web_pages'] else None
+            )
+            for university_data in universities_data
+        ]
 
-
-if __name__ == '__main__':  # TODO DELETE
-    upc = UniversitiesParserCommandImpl(universities_repo=None)
-    upc()
+        self.universities_repo.bulk_create(universities)
